91b2e3f6e64c911a97be59881ac1e3a9426c90f9
Ken Tsen <tsen1220.us@gmail.com>
2020-06-15 15:20:49 +0800

    First commit

---
+++ Assets/Editor/AssetBundlesBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class AssetsBundleBuilder : MonoBehaviour
+{
+	[MenuItem("Assets/Build AssetBundles")]
+	public static void BuildAllAssetBundles()
+	{
+		string directory = "Assets/AssetBundles";
+		if (!Directory.Exists(directory))
+		{
+			Directory.CreateDirectory(directory);
+		}
+#if UNITY_STANDALONE_OSX
+		BuildPipeline.BuildAssetBundles(directory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneOSX);
+#elif UNITY_IOS
+		BuildPipeline.BuildAssetBundles(directory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.iOS);
+#elif UNITY_ANDROID
+		BuildPipeline.BuildAssetBundles(directory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.Android);
+#elif UNITY_STANDALONE_WIN
+		BuildPipeline.BuildAssetBundles(directory, BuildAssetBundleOptions.ChunkBasedCompression, BuildTarget.StandaloneWindows);
+#endif
+	}
+}
---
+++ Assets/Scripts/LoadAssetBundleFromFile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class LoadAssetBundleFromFile : MonoBehaviour
+{
+	public GameObject parents;
+
+	private void Start()
+	{
+		AssetBundle loginBundle = AssetBundle.LoadFromFile(Path.Combine(Application.dataPath, "AssetBundles", "loginbutton"));
+
+		if (loginBundle == null)
+		{
+			Debug.Log("Failed to load AssetBundle!");
+			return;
+		}
+		GameObject prefab = loginBundle.LoadAsset<GameObject>("Login");
+		GameObject login = Instantiate(prefab);
+		login.transform.SetParent(parents.transform);
+		login.transform.localPosition = new Vector2 (-400, 0);
+	}
+}
