36012f62638c936b26671b34df1296a516def4af
Ken Tsen <tsen1220.us@gmail.com>
2020-06-15 15:54:24 +0800

    Sprites AssetBundle

--- Assets/Scripts/LoadAssetBundleFromFile.cs
+++ Assets/Scripts/LoadAssetBundleFromFile.cs
@@ -6,9 +6,11 @@ using System.IO;
 public class LoadAssetBundleFromFile : MonoBehaviour
 {
 	public GameObject parents;
+	public GameObject icon;
 
 	private void Start()
 	{
+		// Load GameObject
 		AssetBundle loginBundle = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AssetBundles", "loginbutton"));
 
 		if (loginBundle == null)
@@ -20,5 +22,16 @@ public class LoadAssetBundleFromFile : MonoBehaviour
 		GameObject login = Instantiate(prefab);
 		login.transform.SetParent(parents.transform);
 		login.transform.localPosition = new Vector2 (-400, 0);
+
+		// Load Sprite
+		AssetBundle loginImage = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "AssetBundles", "loginimage"));
+
+		if (loginImage == null)
+		{
+			Debug.Log("Failed to load AssetBundle!");
+			return;
+		}
+		Sprite image = loginImage.LoadAsset<Sprite>("line-icon");
+		icon.GetComponent<UnityEngine.UI.Image>().sprite = image;
 	}
 }
